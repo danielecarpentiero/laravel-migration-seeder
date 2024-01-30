bd75c852780e54789b32a7c1c4597d90bf121da2
danielecarpentiero <carpentierodaniele@gmail.com>
2024-01-30 17:00:59 +0100

    feat: model and controller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <php? @vite('resources/js/app.js') ?>
+    <php? @vite('resources/js/app.js')
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Template</title>
